6b12c657a892f25230bf3060b6ee8d3b211ac8d6
Timothy Song <tsong0220@gmail.com>
2018-02-21 12:58:30 -0700

    updated validateKeys functions

--- validateObjKeys.js
+++ validateObjKeys.js
@@ -11,25 +11,34 @@ const objectA = {
   
   // running the function with `objectB` and `expectedKeys`
   // should return `false`
-  //const objectB = {
- //   id: 3,
-   // age: 33,
- //   city: 'Peoria',
- // };
+  const objectB = {
+   id: 3,
+   age: 33,
+   city: 'Peoria',
+ };
   
- // const expectedKeys = ['id', 'name', 'age', 'city'];
+ const expectedKeys = ['id', 'name', 'age', 'city'];
   
-//  function validateKeys(object, expectedKeys) {
- //   console.log(Object.keys(objectA)); // returns array of objectA keys
 
- //   }
-//  }
-//
-  console.log(Object.keys(objectA));
+ function validateKeys(object, expectedKeys) {
+   if(Object.keys(objectA).length !== expectedKeys.length ){
+      return false;
+   }
+  for(let i=0; i < expectedKeys.length; i++) {
+    if(!Object.keys(object).find(key => key === expectedKeys[i]))
+    {
+      return false;
+        }
+  }
+   return true 
+   }
+ 
+
+  console.log(validateKeys(objectB, expectedKeys));
 
 
-// array like object
-var obj = { 0: 'a', 1: 'b', 2: 'c' };
-console.log(Object.keys(obj)); // console: ['0', '1', '2']
+// // array like object
+// var obj = { 0: 'a', 1: 'b', 2: 'c' };
+// console.log(Object.keys(obj)); // console: ['0', '1', '2']
 
   
