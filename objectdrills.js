12a276b8f2b49334ce6a0debaaf1399be5b13518
Timothy Song <tsong0220@gmail.com>
2018-02-21 09:27:45 -0700

    added two functions

---
+++ objectdrills.js
@@ -0,0 +1,25 @@
+function createMyObject() {
+    // your code here
+   const obj = {
+      foo : 'bar',
+      answerToUniverse: 42,
+      'olly olly': 'oxen free',
+      sayHello: function () {
+        return 'hello';
+      }
+      
+    }
+  
+    return obj;
+  }
+
+  function updateObject(obj) {
+    // your code here
+    obj.foo = 'foo';
+    obj.bar = 'bar';
+    obj.bizz = 'bizz';
+    obj. bang = 'bang';
+    return obj;
+  }
+
+  
